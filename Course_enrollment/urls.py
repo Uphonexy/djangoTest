49587f6490d9b251b1ca0d97ec4efc2515f004f6
Uphonexy <maharshitripathi302@gmail.com>
2026-02-02 12:47:15 +0530

    Add root URL redirect to courses

--- Course_enrollment/urls.py
+++ Course_enrollment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='courses/', permanent=False)),
     path('courses/', views.CourseListView.as_view(), name='course-list'),
     path('courses/<int:pk>/', views.CourseRetrieveUpdateDestroyView.as_view(), name='course-detail'),
     path('courses/<int:course_id>/modules/', views.ModuleListCreateView.as_view(), name='module-list'),
